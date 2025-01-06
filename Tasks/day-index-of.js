539f7971af7f934f8208a5d05ec6aca768e7b4f6
Ann Kuts <kuutsann@gmail.com>
2025-01-06 15:09:18 +0200

    refactor(day-index-of.js): added indexOf method

--- Tasks/day-index-of.js
+++ Tasks/day-index-of.js
@@ -1,17 +1,26 @@
 // Refactor following solution
 // Get day number
 // Step 0: Used Prettier
+// Step 1: Added "use strict"
+// Step 2: Added const and let
+// Step 3: Changed namings to Camel case
+// Step 4: Changed for-loop to indexOf method
 
-let D = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+"use strict";
 
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
-      return i + 1;
-    }
-  }
-  return -1;
+const days = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+
+const parseDay = (s) => {
+  const dayIndex = days.indexOf(s);
+  return dayIndex >= 0 ? dayIndex + 1 : -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
