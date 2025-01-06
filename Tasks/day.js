5c773d0322b756df91c472b8370eca5529311840
Ann Kuts <kuutsann@gmail.com>
2025-01-06 12:57:58 +0200

    fix(day.js): added const and let

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,13 +2,14 @@
 // Get day number
 // Step 0: Used Prettier
 // Step 1: Added "use strict"
+// Step 2: Added const and let
+
 "use strict";
 
-let D = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const D = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
+const _parse_day_ = (s) => {
+  for (let i = 0; i < D.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) {
       return i + 1;
     }
