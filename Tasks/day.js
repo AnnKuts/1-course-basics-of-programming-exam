14dddbc6346fec93f53d895f653b9e841ef26250
Ann Kuts <kuutsann@gmail.com>
2025-01-06 13:12:15 +0200

    refactor(day.js): changed for-loop

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,6 +4,7 @@
 // Step 1: Added "use strict"
 // Step 2: Added const and let
 // Step 3: Changed namings to Camel case
+// Step 4: Changed for-loop
 
 "use strict";
 
@@ -19,9 +20,7 @@ const days = [
 
 const parseDay = (s) => {
   for (let i = 0; i < days.length; i++) {
-    if (s.startsWith(days[i].toLowerCase())) {
-      return i + 1;
-    }
+    if (s.startsWith(days[i])) return i + 1;
   }
   return -1;
 };
