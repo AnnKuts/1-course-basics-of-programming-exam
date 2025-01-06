6622dcf18dc2b66fdc5b6dd2aef9993eb5a0b4e3
Ann Kuts <kuutsann@gmail.com>
2025-01-06 15:03:38 +0200

    refactor(day-find-index.js): added findIndex method

--- Tasks/day-find-index.js
+++ Tasks/day-find-index.js
@@ -1,17 +1,26 @@
 // Refactor following solution
 // Get day number
 // Step 0: Used Prettier
+// Step 1: Added "use strict"
+// Step 2: Added const and let
+// Step 3: Changed namings to Camel case
+// Step 4: Changed for-loop to findIndex method
 
-let D = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+"use strict";
 
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
-      return i + 1;
-    }
-  }
-  return -1;
+const days = [
+  "sunday",
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+];
+
+const parseDay = (s) => {
+  const dayIndex = days.findIndex((day) => day === s);
+  return dayIndex === -1 ? dayIndex : dayIndex + 1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
