ae9bddbca900c1e5fb6720b478105f802706d7f6
Ann Kuts <kuutsann@gmail.com>
2025-01-06 14:56:59 +0200

    chore: created day-find-index.js and day-index-of.js

---
+++ Tasks/day-index-of.js
@@ -0,0 +1,17 @@
+// Refactor following solution
+// Get day number
+// Step 0: Used Prettier
+
+let D = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+_parse_day_ = (s) => {
+  let i;
+  for (i = 0; i < D.length; i++) {
+    if (s.startsWith(D[i].toLowerCase())) {
+      return i + 1;
+    }
+  }
+  return -1;
+};
+
+module.exports = _parse_day_;
