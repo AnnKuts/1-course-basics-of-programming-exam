070392cd3a5595ca70975c9a7a81aee946f9dfda
Ann Kuts <kuutsann@gmail.com>
2025-01-11 16:00:46 +0200

    refactor(range.js): changed naming

--- Tasks/range.js
+++ Tasks/range.js
@@ -3,19 +3,20 @@
 // Step 0: Used eslint and prettier
 // Step 1: Added "use strict"
 // Step 2: Added const, declared function
+// Step 3: Changed naming, switched to CamelCase
 "use strict";
 
-const _range = (...Range) => {
-  const [from, to] = Range;
+const range = (...range) => {
+  const [from, to] = range;
   if (to >= from) {
-    Range = new Array(to - from + 1);
+    range = new Array(to - from + 1);
     for (let i = from; i <= to; i++) {
-      Range[i - from] = i;
+      range[i - from] = i;
     }
   } else {
     return [];
   }
-  return Range;
+  return range;
 };
 
-module.exports = _range;
+module.exports = range;
