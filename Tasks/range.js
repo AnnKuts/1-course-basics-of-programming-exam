0d4113a5ec9de2759a21e0769b0cb716ce0edf98
Ann Kuts <kuutsann@gmail.com>
2025-01-11 16:06:06 +0200

    refactor(range.js): declared and initialized result

--- Tasks/range.js
+++ Tasks/range.js
@@ -4,19 +4,20 @@
 // Step 1: Added "use strict"
 // Step 2: Added const, declared function
 // Step 3: Changed naming, switched to CamelCase
+// Step 4: Declared and initialized result
 "use strict";
 
 const range = (...range) => {
   const [from, to] = range;
+  const result = [];
   if (to >= from) {
     range = new Array(to - from + 1);
     for (let i = from; i <= to; i++) {
       range[i - from] = i;
     }
   } else {
-    return [];
+    return result;
   }
-  return range;
 };
 
 module.exports = range;
