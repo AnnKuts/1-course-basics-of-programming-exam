913098cbaab241583b87e8b503b3e3f2f1e6db33
Ann Kuts <kuutsann@gmail.com>
2025-01-11 16:09:04 +0200

    refactor(range.js): added push method

--- Tasks/range.js
+++ Tasks/range.js
@@ -5,19 +5,16 @@
 // Step 2: Added const, declared function
 // Step 3: Changed naming, switched to CamelCase
 // Step 4: Declared and initialized result
+// Step 5: Added push method
 "use strict";
 
 const range = (...range) => {
   const [from, to] = range;
   const result = [];
-  if (to >= from) {
-    range = new Array(to - from + 1);
-    for (let i = from; i <= to; i++) {
-      range[i - from] = i;
-    }
-  } else {
-    return result;
+  for (let i = from; i <= to; i++) {
+    result.push(i);
   }
+  return result;
 };
 
 module.exports = range;
