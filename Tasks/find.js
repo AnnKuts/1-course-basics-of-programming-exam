d77e675dbfd446552bee5723f9df553f34bcf993
Ann Kuts <kuutsann@gmail.com>
2025-01-12 14:57:44 +0200

    fix(find.js): added "use strict"

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,5 +1,8 @@
 // Refactor following solution
 // Find key by value
+// Step 0: Used eslint and prettier
+// Step 1: Added "use strict"
+"use strict";
 
 find = (object, ...rest) => {
   value = rest.pop(1);
