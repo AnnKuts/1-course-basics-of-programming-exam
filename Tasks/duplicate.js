df0ed890b00b2ef988dc9be03e7e6937385e5548
Ann Kuts <kuutsann@gmail.com>
2025-01-12 14:35:14 +0200

    fix(duplicate.js): added "use strict"

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,5 +1,8 @@
 // Refactor following solution
 // Return an array without duplicates
+// Step 0: Used eslint and prettier
+// Step 1: Added "use strict"
+"use strict";
 
 duplicate = (value, N) => {
   if (N <= 0) return [];
