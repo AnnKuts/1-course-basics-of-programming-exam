d359f5ea368f02bdf23819add91796689e39d87f
Ann Kuts <kuutsann@gmail.com>
2025-01-13 20:29:54 +0200

    refactor(day-for.js): changed naming

--- Tasks/day-for.js
+++ Tasks/day-for.js
@@ -18,9 +18,9 @@ const days = [
   "saturday",
 ];
 
-const parseDay = (s) => {
+const parseDay = (string) => {
   for (let i = 0; i < days.length; i++) {
-    if (s.startsWith(days[i])) return i + 1;
+    if (string.startsWith(days[i])) return i + 1;
   }
   return -1;
 };
